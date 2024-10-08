b5ab1e5711ef08db41feeb3c73ab32215a5ae0de
sarahrainsberger <sarahrainsberger@gmail.com>
2024-10-08 15:14:40 +0000

    start build structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,12 +48,13 @@ export default defineConfig({
 						  {
 							label: 'Basics',
 							items: [
-								{label: "Projectc Structure", link:'concepts/example'},
-								{label: "Components", link:'concepts/example'},
-								{label: "Pages", link:'concepts/example'},
-								{label: "Layouts", link:'concepts/example'},
-								{label: "Astro Template Syntax", link:'concepts/example'},
-								{label: "Rendering Modes", link:'concepts/example'},
+								{ label: "Projectc Structure", link:'concepts/example'},
+								{ label: "Components", link:'concepts/example'},
+								{ label: "Pages", link:'concepts/example'},
+								{ label: "Layouts", link:'concepts/example'},
+								{ label: "Astro Template Syntax", link:'concepts/example'},
+								{ label: "Rendering Modes", link:'concepts/example'},
+								{ label: "Routing", slug: 'basics/example' },
 							],
 						  },
 						  { label: "Community Resources", slug: 'basics/example' },
@@ -61,7 +62,69 @@ export default defineConfig({
 				},
 				{
 					label: 'Build',
-					autogenerate: { directory: 'reference' },
+					items: [
+					{ label:"Popular Topics", items: [
+						{ label: "Images", slug: 'basics/example' },
+						{ label: "Content Collections", link:'concepts/example'},
+						{ label: "CSS & Styling", slug: 'basics/example' },
+						{ label: "On-demand Rendering", slug: 'basics/example' },
+						{ label: "Scripts & Event Handling", slug: 'basics/example' },
+						{ label: "Markdown", slug: 'basics/example' },
+						{ label: "UI Frameworks", slug: 'basics/example' },
+						],
+					},
+					  {
+						label: 'Guides',
+						items: [
+							
+							{ label: "View Transitions", link:'concepts/example'},
+							{ label: "Server Islands", link:'concepts/example'},
+							{ label: "Middleware", link:'concepts/example'},
+							{ label: "Internationalization", link:'concepts/example'},
+							{ label: "Fonts", link:'concepts/example'},
+							{ label: "Data Fetching", link:'concepts/example'},
+							{ label: "Prefetch", link:'concepts/example'},
+							{ label: "Astro DB", link:'concepts/example'},
+							{ label: "Testing", link:'concepts/example'},
+							{ label: "Actions", link:'concepts/example'},
+							{ label: "Actions", link:'concepts/example'},
+							{ label: "Actions", link:'concepts/example'},
+							{ label: "Actions", link:'concepts/example'},
+						],
+					  },
+					  {
+						label: 'Use Cases',
+						items: [
+							{label: "E-commerce", link:'concepts/example'},
+							{label: "Auth", link:'concepts/example'},
+							{label: "Testing", link:'concepts/example'},
+		
+						],
+					  },
+					  {
+						label: 'Third-party guides',
+						items: [
+							{
+								label: 'CMS guides',
+								items: [
+									{label: "E-commerce", link:'concepts/example'},
+									{label: "Auth", link:'concepts/example'},
+									{label: "Testing", link:'concepts/example'},
+				
+								],
+							  },
+							  {
+								label: 'Backend Services',
+								items: [
+									{label: "E-commerce", link:'concepts/example'},
+									{label: "Auth", link:'concepts/example'},
+									{label: "Testing", link:'concepts/example'},
+				
+								],
+							  },
+						],
+					  },
+					],
 				},
 				{
 					label: 'Reference',
