ee9a72bc01a5d7e5243cc4b070e5da38b5f7ad36
sarahrainsberger <sarahrainsberger@gmail.com>
2024-10-08 16:42:37 +0000

    more start and build

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,73 +26,78 @@ export default defineConfig({
 						{ label:"Getting Started", items: [
 							{ label: "Installation", slug: 'basics/example' },
 							{ label: "Deploy Your Site", slug: 'basics/example' },
-							{
-								label: 'Project setup',
-								items: [
-								{label: "The Astro Config File", link:'basics/example'},
-								{label: "Editor setup", link:'basics/example'},
-								{label: "TypeScript", link:'basics/example'},
-								{label: "Environment variables", link:'basics/example'},
-								{label: "Dev Toolbar", link:'basics/example'},
-								],
-							},	
-							{
-								label: 'Integrations',
-								items: [
-									{label: "Adding Integrations", link:'basics/example'},
-									{
-										label: 'Popular',
-										collapsed: true,
-										items: [
-											{label: "Tailwind", link:'basics/example'},	
-											{label: "React", link:'basics/example'},
-											{label: "Sitemap", link:'basics/example'},
-											{label: "MDX", link:'basics/example'},
-										],
-									},
-								],
-							},
+							{ label: "Migrate to Astro", slug: 'basics/example' },
+							{ label: "Tutorial: Build a Blog", slug: 'basics/example' },
 							],
 						},
+						{
+							label: 'Project setup',
+							items: [
+							{label: "The Astro Config File", link:'basics/example'},
+							{label: "Editor setup", link:'basics/example'},
+							{label: "TypeScript", link:'basics/example'},
+							{label: "Environment variables", link:'basics/example'},
+							{label: "Dev Toolbar", link:'basics/example'},
+							],
+						},
+						{
+							label: 'Add Integrations',
+							items: [
+								{label: "Astro Integrations", link:'basics/example'},
+								{
+									label: 'Popular Integrations',
+									items: [
+										{label: "Tailwind", link:'basics/example'},	
+										{label: "React", link:'basics/example'},
+										{label: "Sitemap", link:'basics/example'},
+										{label: "MDX", link:'basics/example'},
+									],
+								},
+							],
+						},
+						{ label:"Core Concepts", items: [
 						  {
-							label: 'Concepts',
+							label: 'About Astro',
 							items: [
 								{label: "Why Astro?", link:'concepts/example'},
 								{label: "Astro Islands", link:'concepts/example'},
 							],
 						  },
 						  {
-							label: 'Basics',
+							label: 'Astro Basics',
 							items: [
-								{ label: "Projectc Structure", link:'concepts/example'},
+								{ label: "Project Structure", link:'concepts/example'},
 								{ label: "Components", link:'concepts/example'},
 								{ label: "Pages", link:'concepts/example'},
 								{ label: "Layouts", link:'concepts/example'},
 								{ label: "Astro Template Syntax", link:'concepts/example'},
 								{ label: "Rendering Modes", link:'concepts/example'},
-								{ label: "Routing", slug: 'basics/example' },
 							],
 						  },
+						],
+					},
 						  { label: "Community Resources", slug: 'basics/example' },
 					],
 				},
 				{
 					label: 'Build',
 					items: [
-						{ label:"Popular Topics", items: [
-							{ label: "Images", slug: 'basics/example' },
-							{ label: "Content Collections", link:'concepts/example'},
-							{ label: "CSS & Styling", slug: 'basics/example' },
-							{ label: "On-demand Rendering", slug: 'basics/example' },
-							{ label: "Scripts & Event Handling", slug: 'basics/example' },
-							{ label: "Markdown", slug: 'basics/example' },
-							{ label: "UI Frameworks", slug: 'basics/example' },
-							],
-						},
+					
 					  {
-						label: 'Guides',
+						label: 'Topic Guides',
 						items: [
-							
+							{ label:"Popular Topics", items: [
+								{ label: "Images", slug: 'basics/example' },
+								{ label: "Content Collections", link:'concepts/example'},
+								{ label: "CSS & Styling", slug: 'basics/example' },
+								{ label: "On-demand Rendering", slug: 'basics/example' },
+								{ label: "Routing", slug: 'basics/example' },
+								{ label: "Scripts & Event Handling", slug: 'basics/example' },
+								{ label: "Markdown", slug: 'basics/example' },
+								{ label: "UI Frameworks", slug: 'basics/example' },
+								],
+							},
+							{ label:"More Topics", items: [
 							{ label: "View Transitions", link:'concepts/example'},
 							{ label: "Server Islands", link:'concepts/example'},
 							{ label: "Middleware", link:'concepts/example'},
@@ -108,32 +113,32 @@ export default defineConfig({
 							{ label: "Auth", link:'concepts/example'},
 							{ label: "Troubleshooting", link:'concepts/example'},
 						],
+					},
+						],
 					  },
 					  {
 						label: 'Third-party guides',
 						items: [
-							{ label: 'CMS guides', link:'concepts/example'},
-							{ label: 'Backend Services', link:'concepts/example'},
-							{ label: 'Hosted Media & DAM', link:'concepts/example'},
+							
 							{
-								label: 'Popular',
-								collapsed: true,
+								label: 'Popular Third-party Guides',
 								items: [
 									{label: "Storyblok", link:'basics/example'},	
 									{label: "Cloudinary", link:'basics/example'},
 									{label: "Sentry", link:'basics/example'},
 								],
 							},
-							
+							{ label: 'CMS guides', link:'concepts/example'},
+							{ label: 'Backend Services', link:'concepts/example'},
+							{ label: 'Hosted Media & DAM', link:'concepts/example'},
 						],
 					  },
 					  {
 						label: 'How-to Recipes',
 						items: [
-							{label: "Astro Recipes", link:'basics/example'},
+							
 							{
-								label: 'Popular',
-								collapsed: true,
+								label: 'Popular Recipes',
 								items: [
 									{label: "Dynamically importing images", link:'basics/example'},	
 									{label: "Sharing state between islands", link:'basics/example'},
@@ -142,6 +147,7 @@ export default defineConfig({
 									{label: "Style rendered Markdown with Tailwind", link:'basics/example'},
 								],
 							},
+							{label: "All Recipes", link:'basics/example'},
 						],
 					},
 					],
