4c18433ae56a3cb5bd8287e91f09b584785f7496
sarahrainsberger <sarahrainsberger@gmail.com>
2024-10-18 15:51:39 +0000

    more topics, less nesting

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,71 +14,51 @@ export default defineConfig({
 			customCss: [
 				'./src/styles/custom.css',
 			  ],
-				plugins: [
-					starlightSidebarTopics([
-				
-						{
-					label: 'Start',
-					link: '/why-astro/',
-					icon: 'rocket',
-					items: [
-						// Each item here is one entry in the navigation menu.
+			plugins: [
+				starlightSidebarTopics([
+			
+				{
+				label: 'Start',
+				link: '/why-astro/',
+				icon: 'rocket',
+				items: [
+					{ label: "Why Astro?", link:'why-astro'},
+					{ label:"Getting Started", items: [
 						
-						{ label: "Why Astro?", link:'why-astro'},
-
-						{ label:"Getting Started", items: [
+						{ label: "Installation", slug: 'install-and-setup' },
+						{ label: "Project Structure", link:'basics/project-structure'},
+						{ label: "Deploy Your Site", slug: 'guides/deploy' },
+						{ label: "Migrate to Astro", slug: 'guides/migrate-to-astro' },
+						{ label: "Tutorial: Build a Blog", slug: 'tutorial' },
+						],
+					},
+					{
+						label: 'Core Concepts',
+						items: [
 							
-							{ label: "Installation", slug: 'install-and-setup' },
-							{ label: "Project Structure", link:'basics/project-structure'},
-							{ label: "Deploy Your Site", slug: 'guides/deploy' },
-							{ label: "Migrate to Astro", slug: 'guides/migrate-to-astro' },
-							{ label: "Tutorial: Build a Blog", slug: 'tutorial' },
-							],
-						},
-						{
-							label: 'Configuration',
-							items: [
-							{label: "The Astro Config File", link:'guides/configuring-astro'},
-							{label: "Editor setup", link:'editor-setup'},
-							{label: "TypeScript", link:'guides/typescript'},
-							{label: "Environment variables", link:'guides/environment-variables'},
-							{label: "Dev Toolbar", link:'guides/dev-toolbar'},
-							],
-						},
-						{
-							label: 'Core Concepts',
-							items: [
-								
-								{ label: "Astro Islands", link:'basics/islands'},
-								{ label: "Components", link:'basics/astro-components'},
-								{ label: "Pages", link:'basics/astro-pages'},
-								{ label: "Layouts", link:'basics/layouts'},
-								{ label: "Astro Template Syntax", link:'basics/astro-syntax'},
-								
-								
-							],
-						},
-						{
-							label: 'Add Integrations',
-							items: [
-								{
-									label: 'Popular Integrations',
-									items: [
-										{label: "Tailwind", link:'guides/integration/tailwind'},	
-										{label: "React", link:'guides/integration/react'},
-										{label: "Sitemap", link:'guides/integration/sitemap'},
-										{label: "MDX", link:'guides/integration/mdx'},
-									],
-								},
-								{label: "Integrations Guide", link:'guides/integrations-guide'},
-							],
-						},
-						  							
-						 { label: "Community", slug: 'astro-community' },
-					],
+							{ label: "Astro Islands", link:'basics/islands'},
+							{ label: "Components", link:'basics/astro-components'},
+							{ label: "Pages", link:'basics/astro-pages'},
+							{ label: "Layouts", link:'basics/layouts'},
+							{ label: "Astro Template Syntax", link:'basics/astro-syntax'},
+							
+							
+						],
+					},
+					{
+						label: 'Configuration',
+						items: [
+						{label: "The Astro Config File", link:'guides/configuring-astro'},
+						{label: "Editor setup", link:'editor-setup'},
+						{label: "TypeScript", link:'guides/typescript'},
+						{label: "Environment variables", link:'guides/environment-variables'},
+						{label: "Dev Toolbar", link:'guides/dev-toolbar'},
+						],
+					},							
+				],
 				},
 				{
-					label: 'Guides',
+					label: 'Guides and Recipes',
 					link: 'upgrade-astro',
 					icon: 'open-book',
 					items: [
@@ -123,27 +103,7 @@ export default defineConfig({
 							{ label: "Troubleshooting", link:'guides/troubleshooting'},
 						],
 					  },
-					  {
-						label: 'Connect to third-party tools',
-						collapsed:true,
-						items: [
-							
-							{
-								label: 'Popular Third-party Guides',
-								items: [
-									{label: "Storyblok", link:'guides/third-party/storyblok'},	
-									{label: "Cloudinary", link:'guides/third-party/cloudinary'},
-									{label: "Sentry", link:'guides/third-party/sentry'},
-								],
-							},
-							{ label: 'CMS guides', link:'guides/cms'},
-							{ label: 'Backend Services', link:'guides/backend'},
-							{ label: 'Hosted Media & DAM', link:'guides/media'},
-							{ label: "E-commerce", link:'guides/ecommerce'},
-							{ label: "Auth", link:'guides/authentication'},
-							{ label: "Testing", link:'guides/testing'},
-						],
-					  },
+					  
 					  {
 						label: 'How-to Recipes',
 						items: [
@@ -161,7 +121,7 @@ export default defineConfig({
 							},
 							{label: "All Recipes", link:'guides/how-to-recipes'},
 						],
-					},
+					  },
 					],
 				},
 				{
@@ -169,47 +129,143 @@ export default defineConfig({
 					link: '/reference/configuration-reference/',
 					icon: 'seti:info',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						
-						{ label:"User Reference", items: [
-							{ label: "Configuration Reference", slug: 'reference/configuration-reference' },
-							{ label: "Astro CLI Reference", slug: 'reference/cli-reference' },
-							{ label: "Directives Reference", slug: 'reference/directives-reference' },
-							{ label: "Imports", link:'guides/imports'},
-							{ label: "Error Reference", slug: 'reference/error-reference' },						
+						{ 
+							label:"User Reference",
+							items: [
+								{ label: "Configuration Reference", slug: 'reference/configuration-reference' },
+								{ label: "Astro CLI Reference", slug: 'reference/cli-reference' },
+								{ label: "Directives Reference", slug: 'reference/directives-reference' },
+								{ label: "Imports", link:'guides/imports'},
+								{ label: "Error Reference", slug: 'reference/error-reference' },						
 							],
 						},
 						{
 							label: 'Astro Modules',
 							items: [
-							{label: "The Astro Global", slug: 'reference/api-reference' },
-							{label: "`astro:actions`", link:'reference/actions-module'},
-							{label: "`astro:assets`", link:'reference/assets-module'},
-							{label: "`astro:content`", link:'reference/content-module'},
-							{label: "`astro:env`", link:'reference/env-module'},
-							{label: "`astro:i18n`", link:'reference/i18n-module'},
-							{label: "`astro:middleware`", link:'reference/middleware-module'},
-							{label: "`astro:transitions/client`", link:'reference/transitions-module'},
+								{label: "The Astro Global", slug: 'reference/api-reference' },
+								{label: "`astro:actions`", link:'reference/actions-module'},
+								{label: "`astro:assets`", link:'reference/assets-module'},
+								{label: "`astro:content`", link:'reference/content-module'},
+								{label: "`astro:env`", link:'reference/env-module'},
+								{label: "`astro:i18n`", link:'reference/i18n-module'},
+								{label: "`astro:middleware`", link:'reference/middleware-module'},
+								{label: "`astro:transitions/client`", link:'reference/transitions-module'},
 							],
 						},
 						{
 							label: 'Other Development APIs',
 							items: [
-							{label: "Integrations API", link:'reference/integrations-reference'},
-							{label: "Adapter API", link:'reference/adapter-reference'},
-							{label: "Image Service API", link:'reference/image-service-reference'},
-							{label: "Content Loader API", link:'reference/content-loader-reference'},
-							{label: "Dev Toolbar App API", link:'reference/dev-toolbar-app-reference'},
-							{label: "Container API (Experimental)", link:'reference/container-reference'},
-							{label: "Publish to NPM", link:'reference/publish-to-npm'},
+								{label: "Integrations API", link:'reference/integrations-reference'},
+								{label: "Adapter API", link:'reference/adapter-reference'},
+								{label: "Image Service API", link:'reference/image-service-reference'},
+								{label: "Content Loader API", link:'reference/content-loader-reference'},
+								{label: "Dev Toolbar App API", link:'reference/dev-toolbar-app-reference'},
+								{label: "Container API (Experimental)", link:'reference/container-reference'},
+								{label: "Publish to NPM", link:'reference/publish-to-npm'},
 							],
 						},
 					],
 				},
 				{
-					label: "CHANGELOG",
-					link: 'https://github.com/withastro/astro/blob/main/packages/astro/CHANGELOG.md',
-					icon: 'astro',
+					label: 'Add Integrations',
+					link: '/guides/integrations-guide/',
+					icon: 'puzzle',
+					items: [
+						{ label: "Integrations Guide", link:'guides/integrations-guide'},
+						{
+							label: 'Popular Integrations',
+							items: [
+								{label: "Tailwind", link:'guides/integration/tailwind'},	
+								{label: "React", link:'guides/integration/react'},
+								{label: "Sitemap", link:'guides/integration/sitemap'},
+								{label: "MDX", link:'guides/integration/mdx'},
+							],
+						},
+						{ label: "All integrations", link:'guides/integrations-guide'},						
+					],
+				},
+				{
+					label: 'Connect to third-party tools',
+					link: '/guides/cms/',
+					icon: 'seti:bicep',
+					items: [
+							
+						{
+							label: 'Popular Third-party Guides',
+							items: [
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+							],
+						},
+						{ 
+							label: 'CMS guides',
+							collapsed: true,
+							items: [
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+							],
+						},
+						{ 
+							label: 'Backend Services',
+							collapsed: true,
+							items: [
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+							],
+						},
+						
+						{ 
+							label: 'Hosted Media & DAM',
+							collapsed: true,
+							items: [
+								{label: "Storyblok", link:'guides/third-party/storyblok'},	
+								{label: "Cloudinary", link:'guides/third-party/cloudinary'},
+								{label: "Sentry", link:'guides/third-party/sentry'},
+							],
+						},
+						{ label: "E-commerce", link:'guides/ecommerce'},
+						{ label: "Auth", link:'guides/authentication'},
+						{ label: "Testing", link:'guides/testing'},
+					],
 				},
 			],
 		)],
